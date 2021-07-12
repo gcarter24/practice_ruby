8037b28f81b17fb18ccc356a92698bbe907b6ecb
George <gchriscarter@gmail.com>
2021-07-12 14:05:04 -0400

    recompleted conditionals, hashes and methods with speed and accuracy

--- methods_practice.rb
+++ methods_practice.rb
@@ -1,52 +1,52 @@
-# # 1
-
-# p "Enter a word:"
-# input = gets.chomp.upcase
-# p input
-# # 2
-
-# p "Enter a number:"
-# input = gets.chomp.to_i
-# if input > 100
-#   p "That's a big number"
-# end
-# # 3
-# p "Enter two numbers"
-# input1 = gets.chomp.to_i
-# input2 = gets.chomp.to_i
-# p input1 + input2
-# # 4
+# 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
 # p "Enter a word"
-# input = gets.chomp.reverse
-# p input
-# # 5
-# p "Enter a number"
-# input = gets.chomp.to_i
-# p input * 10
-# # 6
-# p "Enter two words"
+# word = gets.chomp.upcase
+# p word
+
+# 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
+# p "enter a number"
+# number = gets.chomp.to_i
+# if number > 100
+#   p "That's a big number!"
+# end
+# 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
+# p "Enter two numbers"
+# num1 = gets.chomp.to_i
+# num2 = gets.chomp.to_i
+# p num1 + num2
+# 4. Write a program that asks the user to enter a word, then prints that word in reverse order.
+# p "enter a word"
+# word = gets.chomp
+# p word.reverse
+# 5. Write a program that asks the user to enter a number, then prints the number times 10.
+# p "enter a number"
+# num = gets.chomp.to_i
+# p num * 10
+
+# 6. Write a program that asks the user to enter two words, then prints both words on the same line in all capital letters.
+# p "enter two words"
 # word1 = gets.chomp.upcase
 # word2 = gets.chomp.upcase
-
 # p word1 + " " + word2
-# # 7
-# p "Enter a word"
-# input = gets.chomp.length
-# p input
-# # 8
-# p "Enter a number"
-# input = gets.chomp.to_i
-# if input < 0
+# 7. Write a program that asks the user to enter a word, then prints the number of letters in the word.
+# p "enter a word"
+# word = gets.chomp
+# p word.length
+# 8. Write a program that asks the user to enter a number, then prints "That's a negative number" if the number is less than 0.
+# p "enter a number"
+# num = gets.chomp.to_i
+# if num < 0
 #   p "That's a negative number"
 # end
-# # 9
-# p "enter two numbers"
-# input1 = gets.chomp.to_i
-# input2 = gets.chomp.to_i
-# p input1 * input2
-# # 10
+# 9. Write a program that asks the user to enter two numbers, then prints the two numbers multiplied together.
+# p "enter two nums"
+# num1 = gets.chomp.to_i
+# num2 = gets.chomp.to_i
+# p num1 * num2
+# 10. Write a program that asks the user to enter a word, then prints "That's a long word" if the word has more than 5 letters.
 # p "Enter a word"
-# input = gets.chomp.length
-# if input > 5
-#   p "That's a big word!"
+# word = gets.chomp
+# if word.length > 5
+#   p "That's a long word"
 # end
+# SOLUTIONS: https://gist.github.com/peterxjang/1539a3ad79728ba4fb68dd8d07279c29
--- strings_practice.rb
+++ strings_practice.rb
@@ -1,73 +1,53 @@
-# # 1
-# x = "Chris"
-# y = "Carter"
-
-# p "Hello " + x + " " + y
-
-# # 2
-# x = "Chris"
-# y = "Carter"
-
+# 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
+# x = "tim"
+# y = "allen"
+#
+# p x + " " + y
+# 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+# x = "tim"
+# y = "allen"
 # p "#{x}" + " " + "#{y}"
-
-# # 3
-
-# p "Please enter a word: "
-# input = gets.chomp
-# if input == "marco"
-#   puts "polo"
+# 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
+# p "enter a word"
+# word = gets.chomp
+# if word == "marco"
+#   p "polo"
 # end
-
-# # 4
-
-# b = "blue"
-# r = "red"
-# y = "yellow"
-
-# p "The sky is " + b + " at night it's " + r + " when it storms it's " + y + "."
-
-# # 5
-
-# b = "blue"
-# r = "red"
-# y = "yellow"
-
-# p "The sky is " + "#{b}" + " at night it's " + "#{r}" + " when it storms it's " + "#{y}" + "."
-
-# # 6
-
-# p "Please Enter a name:"
-# input = gets.chomp
-# if input != "Santa"
-#   p "You're not Santa."
+# 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
+# x = "blue"
+# y = "red"
+# z = "white"
+# p "The American flag is " + y + ", " + z + " and " + x
+# 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
+# x = "blue"
+# y = "red"
+# z = "white"
+# p "The American flag is " + "#{y}" + ", " + "#{z}" + " and " + "#{x}"
+# 6. Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
+# p "Enter a name"
+# name = gets.chomp
+# if name != "Santa"
+#   p "You're not Santa!"
 # end
-
-# # 7
-# name = "Fight Club"
-# author = "Chuck P"
-
-# p "a great book is " + name + " by " + author
-
-# # 8
-# name = "Fight Club"
-# author = "Chuck P"
-
-# p "a great book is " + "#{name}" + " by " + "#{author}"
-
-# # 9
-
-# p "Please enter a password:"
-# input = gets.chomp
-
-# if input == "Joshua"
-#   p "Shall we play a game?"
+# 7. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string concatenation (the + operator).
+# x = "GOT"
+# y = "George RR Martin"
+# p x + " was a book that was made into a show for HBO written by " + y
+# 8. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string interpolation (the #{} operator).
+# x = "GOT"
+# y = "George RR Martin"
+# p "#{x}" + " was a book that was made into a show for HBO written by " + "#{y}"
+# 9. Write a program that asks the user to enter a password. If the password is "Joshua", the program responds "Shall we play a game?". For any other password, the program responds "Access denied"
+# p "Enter a password"
+# password = gets.chomp
+# if password == "Joshua"
+# p "Shall we play a game?"
 # else
-#   p "Access Denied"
+# p "Access Denied"
 # end
+# 10. Write a program that uses variables to store the names of three cities, then prints out a sentence using that information with string concatenation (the + operator).
+# x = "Portsmouth"
+# y = "Boston"
+# z = "Hartford"
 
-# # 10
-# n = "New York"
-# h = "Hartford"
-# nh = "New Haven"
-
-# p "I'm traveling and I'm going to visit " + n + ", " + h + ", and " + nh
+# p "I would like to live in " + x + " but right now I live in " + z + " but I'd like to be closer to " + y
