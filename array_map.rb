e23a9d7477bb0f964516449e34877003a4cf14b3
George <gchriscarter@gmail.com>
2021-07-19 16:51:19 -0400

    add nested loops(still needs a lot of work) and redo mapping arrays to improve time and accuracy

--- array_map.rb
+++ array_map.rb
@@ -1,172 +1,161 @@
 #  1. Start with an array of numbers and create a new array with each number times 3.
 #     For example, [1, 2, 3] becomes [3, 6, 9].
-# array = [1, 2, 3]
-# new_array = []
-# array.each do |num|
-#   new_array << num * 3
+# arr = [1, 2, 3]
+# new_arr = []
+# arr.each do |num|
+#   new_arr << num * 3
 # end
-# p new_array
-
-# array = [1, 2, 3]
-# new_array = []
+# p new_arr
+# arr = [1, 2, 3]
+# new_arr = []
 # i = 0
-# while i < array.length
-#   new_array << array[i] * 3
+# while i < arr.length
+#   new_arr << arr[i] * 3
 #   i += 1
 # end
-
-# p new_array
-# p array.map { |num| num * 3 }
+# p new_arr
+# p arr.map { |num| num * 3 }
 #  2. Start with an array of strings and create a new array with each string upcased.
 #     For example, ["hello", "goodbye"] becomes ["HELLO", "GOODBYE"].
-# array = ["hello", "goodbye"]
-# new_array = []
-# array.each do |str|
-#   new_array << str.upcase
+# arr = ["hello", "goodbye"]
+# new_arr = []
+# arr.each do |str|
+#   new_arr << str.upcase
 # end
-# p new_array
-# array = ["hello", "goodbye"]
-# new_array = []
+# p new_arr
+# arr = ["hello", "goodbye"]
+# new_arr = []
 # i = 0
-# while i < array.length
-#   new_array << array[i].upcase
+# while i < arr.length
+#   new_arr << arr[i].upcase
 #   i += 1
 # end
-# p new_array
-# p array.map { |str| str.upcase }
+# p new_arr
+# p arr.map { |str| str.upcase }
 #  3. Start with an array of hashes and create a new array of string values from each hash's :name key.
 #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes ["Alice", "Blane"].
-# array = [{ name: "Alice", age: 27 }, { name: "Blane", age: 16 }]
-# new_array = []
-# array.each do |str|
-#   new_array << str[:name]
+# arr = [{ name: "Alice", age: 27 }, { name: "Blane", age: 16 }]
+# new_arr = []
+# arr.each do |str|
+#   new_arr << str[:name]
 # end
-# p new_array
-# array = [{ name: "Alice", age: 27 }, { name: "Blane", age: 16 }]
-# new_array = []
+# p new_arr
+# arr = [{ name: "Alice", age: 27 }, { name: "Blane", age: 16 }]
+# new_arr = []
 # i = 0
-# while i < array.length
-#   new_array << array[i][:name]
+# while i < arr.length
+#   new_arr << arr[i][:name]
 #   i += 1
 # end
-# p new_array
-# p array.map { |str| str[:name] }
+# p new_arr
+# p arr.map { |str| str[:name] }
 #  4. Start with an array of numbers and create a new array with each number plus 7.
 #     For example, [1, 2, 3] becomes [8, 9, 10].
-# array = [1, 2, 3]
-# new_array = []
-# array.each do |num|
-#   new_array << num + 7
+# arr = [1, 2, 3]
+# new_arr = []
+# arr.each do |num|
+#   new_arr << num + 7
 # end
-# p new_array
-# array = [1, 2, 3]
-# new_array = []
+# p new_arr
+# arr = [1, 2, 3]
+# new_arr = []
 # i = 0
-# while i < array.length
-#   new_array << array[i] + 7
+# while i < arr.length
+#   new_arr << arr[i] + 7
 #   i += 1
 # end
-# p new_array
-# p array.map { |num| num + 7 }
+# p new_arr
+# p arr.map { |num| num + 7 }
 #  5. Start with an array of strings and create a new array with each string's length.
 #     For example, ["hello", "goodbye"] becomes [5, 7].
-# array = ["hello", "goodbye"]
-# new_array = []
-# array.each do |str|
-#   new_array << str.length
+# arr = ["hello", "goodbye"]
+# new_arr = []
+# arr.each do |num|
+#   new_arr << num.length
 # end
-# p new_array
-# array = ["hello", "goodbye"]
-# new_array = []
+# p new_arr
+# arr = ["hello", "goodbye"]
+# new_arr = []
 # i = 0
-# while i < array.length
-#   new_array << array[i].length
+# while i < arr.length
+#   new_arr << arr[i].length
 #   i += 1
 # end
-# p new_array
-# p array.map { |str| str.length }
+# p new_arr
+# p arr.map { |num| num.length }
 #  6. Start with an array of hashes and create a new array of number values from each hash's :age key.
 #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [27, 16].
-# array = [{ name: "Alice", age: 27 }, { name: "Blane", age: 16 }]
-# new_array = []
-# array.each do |num|
-#   new_array << num[:age]
+# arr = [{ name: "Alice", age: 27 }, { name: "Blane", age: 16 }]
+# new_arr = []
+# arr.each do |num|
+#   new_arr << num[:age]
 # end
-# p new_array
-# array = [{ name: "Alice", age: 27 }, { name: "Blane", age: 16 }]
-# new_array = []
+# p new_arr
+# arr = [{ name: "Alice", age: 27 }, { name: "Blane", age: 16 }]
+# new_arr = []
 # i = 0
-# while i < array.length
-#   new_array << array[i][:age]
+# while i < arr.length
+#   new_arr << arr[i][:age]
 #   i += 1
 # end
-# p new_array
-# p array.map { |num| num[:age] }
+# p new_arr
+# p arr.map { |num| num[:age] }
 #  7. Start with an array of numbers and create a new array with each number divided by 2.
 #     For example, [1, 2, 3] becomes [0.5, 1.0, 1.5].
-# array = [1, 2, 3]
-# new_array = []
-# array.each do |num|
-#   new_array << num.to_f / 2
+# arr = [1, 2, 3]
+# new_arr = []
+# arr.each do |num|
+#   new_arr << num.to_f / 2
 # end
-# p new_array
-# array = [1, 2, 3]
-# new_array = []
-# i = 0
-# while i < array.length
-#   new_array << array[i].to_f / 2
-#   i += 1
-# end
-# p new_array
-# p array.map { |num| num.to_f / 2 }
+# p new_arr
 #  8. Start with an array of strings and create a new array with each string's first letter only.
 #     For example, ["hello", "goodbye"] becomes ["h", "g"].
-# array = ["hello", "goodbye"]
-# new_array = []
-# array.each do |str|
-#   new_array << str[0]
+# arr = ["hello", "goodbye"]
+# new_arr = []
+# arr.each do |str|
+#   new_arr << str[0]
 # end
-# p new_array
-# array = ["hello", "goodbye"]
-# new_array = []
+# p new_arr
+# arr = ["hello", "goodbye"]
+# new_arr = []
 # i = 0
-# while i < array.length
-#   new_array << array[i][0]
+# while i < arr.length
+#   new_arr << arr[i][0]
 #   i += 1
 # end
-# p new_array
-# p array.map { |str| str[0] }
+# p new_arr
+# p arr.map { |num| num[0] }
 # 9.  Start with an array of hashes and create a new array of number values from each hash's :age key times 2.
 #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [54, 32].
-# array = [{ name: "Alice", age: 27 }, { name: "Blane", age: 16 }]
-# new_array = []
-# array.each do |num|
-#   new_array << num[:age] * 2
+# arr = [{ name: "Alice", age: 27 }, { name: "Blane", age: 16 }]
+# new_arr = []
+# arr.each do |num|
+#   new_arr << num[:age] * 2
 # end
-# p new_array
-# array = [{ name: "Alice", age: 27 }, { name: "Blane", age: 16 }]
-# new_array = []
+# p new_arr
+# arr = [{ name: "Alice", age: 27 }, { name: "Blane", age: 16 }]
+# new_arr = []
 # i = 0
-# while i < array.length
-#   new_array << array[i][:age] * 2
+# while i < arr.length
+#   new_arr << arr[i][:age] * 2
 #   i += 1
 # end
-# p new_array
-# p array.map { |num| num[:age] * 2 }
+# p new_arr
+# p arr.map { |num| num[:age] * 2 }
 # 10. Start with an array of numbers and create a new array with each number converted into a string.
 #     For example, [1, 2, 3] becomes ["1", "2", "3"].
-# array = [1, 2, 3]
-# new_array = []
-# array.each do |str|
-#   new_array << str.to_s
+# arr = [1, 2, 3]
+# new_arr = []
+# arr.each do |num|
+#   new_arr << num.to_s
 # end
-# p new_array
-# array = [1, 2, 3]
-# new_array = []
+# p new_arr
+# arr = [1, 2, 3]
+# new_arr = []
 # i = 0
-# while i < array.length
-#   new_array << array[i].to_s
+# while i < arr.length
+#   new_arr << arr[i].to_s
 #   i += 1
 # end
-# p new_array
-# p array.map { |str| str.to_s }
+# p new_arr
+# p arr.map { |str| str.to_s }
---
+++ nested_arrays.rb
@@ -0,0 +1,168 @@
+#  1. Use a nested loop to convert an array of number pairs into a single flattened array.
+#     For example, [[1, 3], [8, 9], [2, 16]] becomes [1, 3, 8, 9, 2, 16].
+# arr = [[1, 3], [8, 9], [2, 16]]
+# new_arr = []
+# i = 0
+# while i < arr.length
+#   new_arr << arr[i][0]
+#   new_arr << arr[i][1]
+#   i += 1
+# end
+# p new_arr
+# # OR Dynamically
+# new_arr = []
+# i = 0
+# while i < arr.length
+#   j = 0
+#   while j < arr[i].length
+#     new_arr << arr[i][j]
+#     j += 1
+#   end
+#   i += 1
+# end
+# p new_arr
+#  2. Use a nested loop with two arrays of strings to create a new array of strings with each string combined.
+#     For example, ["a", "b", "c"] and ["d", "e", "f", "g"] becomes ["ad", "ae", "af", "ag", "bd", "be", "bf", "bg", "cd", "ce", "cf", "cg"].
+# arr1 = ["a", "b", "c"]
+# arr2 = ["d", "e", "f", "g"]
+# new_arr = []
+# i = 0
+# while i < arr1.length
+#   j = 0
+#   while j < arr2.length
+#     new_arr << arr1[i] + arr2[j]
+#     j += 1
+#   end
+#   i += 1
+# end
+# p new_arr
+#  3. Use a nested loop with one array of strings to create a new array that contains every combination of each string with every other string in the array.
+#     For example, ["a", "b", "c", "d"] becomes ["ab", "ac", "ad", "ba", "bc", "bd", "ca", "cb", "cd", "da", "db", "dc"].
+# arr = ["a", "b", "c", "d"]
+# new_arr = []
+# i = 0
+# while i < arr.length
+#   j = 0
+#   while j < arr.length
+#     if i != j
+#       new_arr << arr[i] + arr[j]
+#     end
+#     j += 1
+#   end
+#   i += 1
+# end
+# p new_arr
+#  4. Use a nested loop to find the largest product of any two different numbers within a given array.
+#     For example, [5, -2, 1, -9, -7, 2, 6] becomes 63.
+# arr = [5, -2, 1, -9, -7, 2, 6]
+# max = arr[0] * arr[1]
+# i = 0
+# while i < arr.length
+#   j = 0
+#   while j < arr.length
+#     if i != j
+#       product = arr[i] * arr[j]
+#       if product > max
+#         max = product
+#       end
+#     end
+#     j += 1
+#   end
+#   i += 1
+# end
+# p max
+#  5. Use a nested loop to compute the sum of all the numbers in an array of number pairs.
+#     For example, [[1, 3], [8, 9], [2, 16]] becomes 39.
+# arr = [[1, 3], [8, 9], [2, 16]]
+# sum = 0
+# i = 0
+# while i < arr.length
+#   j = 0
+#   while j < arr[i].length
+#     sum = sum + arr[i][j]
+#     j += 1
+#   end
+#   i += 1
+# end
+# p sum
+#  6. Use a nested loop with two arrays of numbers to create a new array of the sums of each combination of numbers.
+#     For example, [1, 2] and [6, 7, 8] becomes [7, 8, 9, 8, 9, 10].
+# arr1 = [1, 2]
+# arr2 = [6, 7, 8]
+# sum = []
+# i = 0
+# while i < arr1.length
+#   j = 0
+#   while j < arr2.length
+#     sum << arr1[i] + arr2[j]
+#     j += 1
+#   end
+#   i += 1
+# end
+# p sum
+#  7. Use a nested loop with an array of numbers to compute an array with every combination of products from each number.
+#     For example, [2, 8, 3] becomes [4, 16, 6, 16, 64, 24, 6, 24, 9].
+# arr = [2, 8, 3]
+# new_arr = []
+# i = 0
+# while i < arr.length
+#   j = 0
+#   while j < arr.length
+#     new_arr << arr[i] * arr[j]
+#     j += 1
+#   end
+#   i += 1
+# end
+# p new_arr
+#  8. Use a nested loop to find the largest sum of any two different numbers within an array.
+#     For example, [1, 8, 3, 10] becomes 18.
+# arr = [1, 8, 3, 10]
+# max = arr[0] + arr[1]
+# i = 0
+# while i < arr.length
+#   j = 0
+#   while j < arr.length
+#     if i != j
+#       sum = arr[i] + arr[j]
+#       if sum > max
+#         max = sum
+#       end
+#     end
+#     j += 1
+#   end
+#   i += 1
+# end
+# p max
+#  9. Use nested loops with an array of numbers to compute a new array containing the first two numbers (from the original array) that add up to the number 10. If there are no two numbers that add up to 10, return false.
+#     For example, [2, 5, 3, 1, 0, 7, 11] becomes [3, 7].
+# arr = [2, 5, 3, 1, 0, 7, 11]
+# result = false
+# i = 0
+# while i < arr.length
+#   j = 0
+#   while j < arr.length
+#     if i != j
+#       if arr[i] + arr[j] == 10 && result == false
+#         result = [arr[i], arr[j]]
+#       end
+#     end
+#     j += 1
+#   end
+#   i += 1
+# end
+# p result
+# 10. Use a nested loop to convert an array of string arrays into a single string.
+#     For example, [["a", "man"], ["a", "plan"], ["a", "canal"], ["panama"]] becomes "amanaplanacanalpanama".
+# arr = [["a", "man"], ["a", "plan"], ["a", "canal"], ["panama"]]
+# string = ""
+# i = 0
+# while i < arr.length
+#   j = 0
+#   while j < arr[i].length
+#     string += arr[i][j]
+#     j += 1
+#   end
+#   i += 1
+# end
+# p string
+# SOLUTIONS: https://gist.github.com/peterxjang/af8985dc4fb07ea14b4bd6ba41cb08f8
