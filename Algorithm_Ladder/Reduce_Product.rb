fd7794fc28ba88672bb5a846879c4bec97f6e64f
George <gchriscarter@gmail.com>
2021-07-20 10:05:54 -0400

    add reverse array and rework reduce product

--- Algorithm_Ladder/Reduce_Product.rb
+++ Algorithm_Ladder/Reduce_Product.rb
@@ -5,14 +5,32 @@
 
 # Explanation: (1 x 2 x 3 x 4) = 24
 
-def product(array)
+# def product(array)
+#   product = 1
+#   i = 0
+#   while i < array.length
+#     product *= array[i]
+#     i += 1
+#   end
+#   return product
+# end
+
+# p product([1, 2, 3, 4])
+
+# arr = [1, 2, 3, 4]
+# product = 1
+
+# arr.each do |num|
+#   product *= num
+# end
+# p product
+
+def num_product(arr)
   product = 1
-  i = 0
-  while i < array.length
-    product *= array[i]
-    i += 1
+  arr.each do |num|
+    product *= num
   end
   return product
 end
 
-p product([1, 2, 3, 4])
+p num_product([1, 2, 3, 4])
---
+++ Algorithm_Ladder/Reverse_Array.rb
@@ -0,0 +1,16 @@
+# Given an array, write a function that returns an array that contains the original array’s values in reverse.
+
+# Input: [1, 2, 3, 4, 5]
+# Output: [5, 4, 3, 2, 1]
+
+def reverse(arr)
+  i = arr.length
+  new_arr = []
+  while i > 0
+    i -= 1
+    new_arr << arr[i]
+  end
+  return new_arr
+end
+
+p reverse([1, 2, 3, 4, 5])
