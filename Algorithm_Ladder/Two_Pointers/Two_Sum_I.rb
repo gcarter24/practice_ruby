a67a37c45689f92792c2c8eefa02ca61b5c05342
George <gchriscarter@gmail.com>
2021-10-18 16:25:44 -0400

    add solution for first part, adding values equaling ten to a new array

--- Algorithm_Ladder/Two_Pointers/Two_Sum_I.rb
+++ Algorithm_Ladder/Two_Pointers/Two_Sum_I.rb
@@ -7,3 +7,22 @@
 
 # Input: [1, 2, 3, 4, 5]
 # Output: false (While 1, 2, 3, and 4 altogether add up to 10, we're seeking just one pair of numbers.)
+
+array = [2, 5, 3, 1, 0, 7, 11]
+# array = [1, 2, 3, 4, 5]
+new_arr = []
+i = array[0]
+
+while i < array.length
+  j = array[1]
+  while j < array.length
+    if array[i] + array[j] == 10
+      new_arr << array[i]
+      new_arr << array[j]
+    end
+    j += 1
+  end
+  i += 1
+end
+
+p new_arr
