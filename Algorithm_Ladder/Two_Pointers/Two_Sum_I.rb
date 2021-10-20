4eb827902a723436320e6dcfbc2ef74eba605949
George <gchriscarter@gmail.com>
2021-10-20 13:58:01 -0400

    refactor new_arr

--- Algorithm_Ladder/Two_Pointers/Two_Sum_I.rb
+++ Algorithm_Ladder/Two_Pointers/Two_Sum_I.rb
@@ -10,19 +10,22 @@
 
 array = [2, 5, 3, 1, 0, 7, 11]
 # array = [1, 2, 3, 4, 5]
-new_arr = []
-i = array[0]
 
-while i < array.length
-  j = array[1]
-  while j < array.length
-    if array[i] + array[j] == 10
-      new_arr << array[i]
-      new_arr << array[j]
+def two_sum(array)
+  new_arr = []
+  i = array[0]
+
+  while i < array.length
+    j = array[1]
+    while j < array.length
+      if array[i] + array[j] == 10
+        new_arr.push(array[i], array[j])
+      end
+      j += 1
     end
-    j += 1
+    i += 1
   end
-  i += 1
+  return new_arr
 end
 
-p new_arr
+p two_sum(array)
