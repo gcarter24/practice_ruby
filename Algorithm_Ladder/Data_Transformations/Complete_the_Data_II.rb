6d6c1a902f1e403ccfe3cc193dd5c395fc432495
George <gchriscarter@gmail.com>
2021-10-27 14:34:05 -0400

    add answer to complete the data II

--- Algorithm_Ladder/Data_Transformations/Complete_the_Data_II.rb
+++ Algorithm_Ladder/Data_Transformations/Complete_the_Data_II.rb
@@ -1,30 +1,55 @@
-Given an array of social media posts and an array of users, return a list of posts (as an array of hashes) that replaces the submitted_by id number as the person's actual name.
+# Given an array of social media posts and an array of users, return a list of posts (as an array of hashes) that replaces the submitted_by id number as the person's actual name.
 
-For example, given this array of posts (note that the submitted_by is an id number):
+# For example, given this array of posts (note that the submitted_by is an id number):
 
-[
-  {title: 'Me Eating Pizza', submitted_by: 231, likes: 1549},
-  {title: 'i never knew how cool i was until now', submitted_by: 989, likes: 3},
-  {title: 'best selfie evar!!!', submitted_by: 111, likes: 1092},
-  {title: 'Mondays are the worst', submitted_by: 403, likes: 644}
+# [
+#   {title: 'Me Eating Pizza', submitted_by: 231, likes: 1549},
+#   {title: 'i never knew how cool i was until now', submitted_by: 989, likes: 3},
+#   {title: 'best selfie evar!!!', submitted_by: 111, likes: 1092},
+#   {title: 'Mondays are the worst', submitted_by: 403, likes: 644}
+# ]
+
+# And this array of users:
+
+# [
+# 	{user_id: 403, name: "Aunty Em"},
+# 	{user_id: 231, name: "Joelle P."},
+# 	{user_id: 989, name: "Lyndon Johnson"},
+# 	{user_id: 111, name: "Patti Q."},
+# ]
+
+# Return the array of posts as follows:
+
+# [
+# {title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
+# {title: 'i never knew how cool i was until now', submitted_by: "Lyndon Johnson", likes: 3},
+# {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
+# {title: 'Mondays are the worst', submitted_by: "Aunty Em", likes: 644}
+# ]
+
+posts = [
+  { title: "Me Eating Pizza", submitted_by: 231, likes: 1549 },
+  { title: "i never knew how cool i was until now", submitted_by: 989, likes: 3 },
+  { title: "best selfie evar!!!", submitted_by: 111, likes: 1092 },
+  { title: "Mondays are the worst", submitted_by: 403, likes: 644 },
 ]
 
-And this array of users:
-
-[
-	{user_id: 403, name: "Aunty Em"},
-	{user_id: 231, name: "Joelle P."},
-	{user_id: 989, name: "Lyndon Johnson"},
-	{user_id: 111, name: "Patti Q."},
+users = [
+  { user_id: 403, name: "Aunty Em" },
+  { user_id: 231, name: "Joelle P." },
+  { user_id: 989, name: "Lyndon Johnson" },
+  { user_id: 111, name: "Patti Q." },
 ]
 
-Return the array of posts as follows:
+def author(posts, users)
+  users.each do |user|
+    posts.each do |post|
+      if user[:user_id] == post[:submitted_by]
+        post[:submitted_by] = user[:name]
+      end
+    end
+  end
+  return posts
+end
 
-[
-{title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
-{title: 'i never knew how cool i was until now', submitted_by: "Lyndon Johnson", likes: 3},
-{title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
-{title: 'Mondays are the worst', submitted_by: "Aunty Em", likes: 644}
-]
-
-
+p author(posts, users)
