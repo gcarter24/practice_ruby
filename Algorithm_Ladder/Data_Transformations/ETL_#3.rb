b3529d5b91df64c7380264fdfe9effd200dfae33
George <gchriscarter@gmail.com>
2021-10-27 17:10:46 -0400

    add solution to etl_#3

--- Algorithm_Ladder/Data_Transformations/ETL_#3.rb
+++ Algorithm_Ladder/Data_Transformations/ETL_#3.rb
@@ -26,6 +26,32 @@
 videos = [
   { title: "How to Make Wood", author_id: 4, views: 6 },
   { title: "How to Seem Perfect", author_id: 4, views: 111 },
-  { title: 'Review of the New "Unbreakable Mug"', author_id: 2, views: 202 },
+  { title: "Review of the New 'Unbreakable Mug'", author_id: 2, views: 202 },
   { title: "Why Pigs Stink", author_id: 1, views: 12 },
 ]
+
+authors = [
+  { id: 1, first_name: "Jazz", last_name: "Callahan" },
+  { id: 2, first_name: "Ichabod", last_name: "Loadbearer" },
+  { id: 3, first_name: "Saron", last_name: "Kim" },
+  { id: 4, first_name: "Teena", last_name: "Burgess" },
+]
+
+def youtube(videos, authors)
+  video_authors = []
+  videos.each do |video|
+    authors.each do |author|
+      title = video[:title]
+      author_id = video[:author_id]
+      views = video[:views]
+      id = author[:id]
+      author_name = "#{author[:first_name]} #{author[:last_name]}"
+      if views >= 100 && author_id == id
+        video_authors << { title: title, views: views, author_name: author_name }
+      end
+    end
+  end
+  return video_authors
+end
+
+p youtube(videos, authors)
